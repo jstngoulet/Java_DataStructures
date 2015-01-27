d8b2cf664439c3cff8054be1df1e44d7d147a829
Jstngoulet <Jstngoulet@localhost>
2015-01-26 19:18:53 -0800

    Created File

---
+++ edu/miracosta/CS113/hw1/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package miracosta.CS113.hw1;
+
+/**
+ * @author Justin
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing");
+	}
+
+}
