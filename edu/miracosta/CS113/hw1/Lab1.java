26ae4115c6b748d50e158ab735b1d4fae955382c
Jstngoulet <Jstngoulet@localhost>
2015-01-26 20:08:16 -0800

    Finished Lab 1;
    The contents of this file iterates from 1 - 100 displaying Buzz and Fuzz as
         directed and outputs the remaining numbers in the console

--- edu/miracosta/CS113/hw1/Lab1.java
+++ edu/miracosta/CS113/hw1/Lab1.java
@@ -14,22 +14,33 @@ public class Lab1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		boolean found = false;
 		
-		for(int i = 0; i < 100; i++)
+		for(int i = 1; i <= 100; i++)
 		{
 			if (i % 3 == 0)
 			{
 				System.out.println("Fuzz " + i);
+				found = true;
 			}
-			if (i % 5 == 0)
+			else if (i % 5 == 0)
 			{
 				System.out.println("Buzz " + i);
+				found = true;
 			}
 			
 			if((i % 3 == 0) && (i % 5 == 0))
 			{
 				System.out.println("FizzBuzz " + i);
+				found = true;
 			}
+			
+			if (!found)
+			{
+				System.out.println(i);
+				found = true;
+			}
+			found = false;
 		}
 
 	}
